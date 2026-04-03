991ae50ed324f395f47e86c93291df361441870e
Laleilish <nfebri622@gmail.com>
2026-04-03 17:23:18 +0700

    Adding Footer file and some fixes on navbar

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,3 +4,12 @@ fetch('../pages/navbar.html')
   .then(data => {
     document.getElementById('navbar-container').innerHTML = data;
   });
+
+
+// Footer
+
+fetch('../pages/footer.html')
+  .then(res => res.text())
+  .then(data => {
+    document.getElementById('footer-container').innerHTML = data;
+  });

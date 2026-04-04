3a928717ecf44d6fd348c0e2cd8b32878c813d51
Laleilish <nfebri622@gmail.com>
2026-04-04 17:05:14 +0700

    add login& register page

--- assets/js/login-script.js
+++ assets/js/login-script.js
@@ -1,21 +1,12 @@
-// Nav Bar
-fetch('pages/navbar.html')
-  .then(res => res.text())
-  .then(data => {
-    document.getElementById('navbar-container').innerHTML = data;
+const phoneInput = document.getElementById('phone-input');
+const btnLanjutkan = document.getElementById('btn-lanjutkan');
 
-    // Inisialisasi hamburger SETELAH HTML selesai di-inject
-    const hamburger = document.querySelector('.hamburger');
-    const navLinks = document.querySelector('.nav-links');
-    const navbar = document.querySelector('.navbar');
-
-    hamburger.addEventListener('click', () => {
-      navLinks.classList.add('active');
-      navbar.classList.add('menu-open');
-    });
-
-    document.querySelector('.close-btn').addEventListener('click', () => {
-      navLinks.classList.remove('active');
-      navbar.classList.remove('menu-open');
-    });
-  });
+phoneInput.addEventListener('input', () => {
+    if (phoneInput.value.trim().length > 0) {
+        btnLanjutkan.classList.add('enabled');
+        btnLanjutkan.disabled = false;
+    } else {
+        btnLanjutkan.classList.remove('enabled');
+        btnLanjutkan.disabled = true;
+    }
+});

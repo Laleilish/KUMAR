3c0cd740ba3306cf0905f87c72fc5c60aedc313f
hamizanraden <hamizanraden@gmail.com>
2026-04-05 18:39:34 +0700

    membuat tampilan awal untuk fitur hidden gem

---
+++ assets/js/hidden_gem.js
@@ -0,0 +1,29 @@
+// Nav Bar
+fetch('pages/navbar.html')
+  .then(res => res.text())
+  .then(data => {
+    document.getElementById('navbar-container').innerHTML = data;
+
+    // Inisialisasi hamburger
+    const hamburger = document.querySelector('.hamburger');
+    const navLinks = document.querySelector('.nav-links');
+    const navbar = document.querySelector('.navbar');
+
+    hamburger.addEventListener('click', () => {
+      navLinks.classList.add('active');
+      navbar.classList.add('menu-open');
+    });
+
+    document.querySelector('.close-btn').addEventListener('click', () => {
+      navLinks.classList.remove('active');
+      navbar.classList.remove('menu-open');
+    });
+  });
+
+// Footer
+fetch('../pages/footer.html')
+  .then(res => res.text())
+  .then(data => {
+    document.getElementById('footer-container').innerHTML = data;
+  });
+  
